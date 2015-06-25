3a017a521fa6c7d151fca35a78499fe08dea4a8b
Evan Kirsch <ekirsch@allegient.com>
2015-06-25 14:13:20 -0400

    Made the Encounter Feed more programmatic
    Note: The controller function which populates the model for this is building everything via hardcoded values at the moment. This will be reworked in future updates

--- The DM's Screen/Controllers/InitiativeController.cs
+++ The DM's Screen/Controllers/InitiativeController.cs
@@ -11,27 +11,116 @@ namespace The_DM_s_Screen.Models
         //
         // GET: /Initiative/
 
+        // TODO: Add Database so we don't have to populate all of this nonsense here
         public ActionResult Tracker()
         {
-            var model = new TurnOrderModel
+            var timeKeeper = new Character()
             {
-                TurnOrder = new List<string>()
+                Name = "The Time Keeper",
+                Portrait = "../../Content/timekeeper.jpg"
+            };
+
+            var pecos = new Character()
+            {
+                Name = "Pecos",
+                Portrait = "../../Content/pecosbill.jpg"
+            };
+
+            var beorn = new Character()
+            {
+                Name = "Beorn Goddart",
+                Portrait = "../../Content/beorn.gif"
+            };
+
+            var aramil = new Character()
+            {
+                Name = "Aramil",
+                Portrait = "../../Content/aramil.jpg"
+            };
+
+            var amper = new Character()
+            {
+                Name = "Amper",
+                Portrait = "../../Content/amper.jpg"
+            };
+
+            var klarg = new Character()
+            {
+                Name = "Klarg",
+                Portrait = "../../Content/klarg.jpg"
+            };
+
+            var model = new EncounterFeedModel
+            {
+                // Hardcoded, should be populated... somehow
+                TurnOrder = new List<Character>()
                 {
-                    "Amper",
-                    "Aramil",
-                    "Beorn Goddart",
-                    "Pecos",
-                    "The Time Keeper",
-                    "Klarg"
+                    beorn, amper, klarg, pecos, aramil, timeKeeper
+                },
+                FeedEntries = new List<EncounterFeedEntry>()
+                {
+                    new EncounterFeedEntry()
+                    {
+                        ActingCharacter = timeKeeper,
+                        Actions = new List<string>()
+                        {
+                            "Casts Time Ray on Klarg, dealing 13 \"time\" (cold) damage"
+                        }
+                    },
+                    new EncounterFeedEntry()
+                    {
+                        ActingCharacter = aramil,
+                        Actions = new List<string>()
+                        {
+                            "Casts \"Goodberry\" and consumes 5 berries, regaining 5 HP",
+                            "Gives 5 berries to Amper"
+                        }
+                    },
+                    new EncounterFeedEntry()
+                    {
+                        ActingCharacter = pecos,
+                        Actions = new List<string>()
+                        {
+                            "Looses an arrow from his bow at Klarg, which was unfortunately deflected by Klarg"
+                        }
+                    },
+                    new EncounterFeedEntry()
+                    {
+                        ActingCharacter = klarg,
+                        Actions = new List<string>()
+                        {
+                            "Wails on Amper with his Morningstar, dealing 16 bludgeoning damage"
+                        }
+                    },
+                    new EncounterFeedEntry()
+                    {
+                        ActingCharacter = amper,
+                        Actions = new List<string>()
+                        {
+                            "Performs a song to bardically inspire Beorn, who may now add 1d8 to any roll of his choice"
+                        }
+                    },
+                    new EncounterFeedEntry()
+                    {
+                        ActingCharacter = beorn,
+                        Actions = new List<string>()
+                        {
+                            "Uses \"Lay on Hands\" to revive Aramil with 1 HP",
+                            "Readies his Shield of Faith and prepares for an attack"
+                        }
+                    },
+                    new EncounterFeedEntry()
+                    {
+                        ActingCharacter = timeKeeper,
+                        Actions = new List<string>()
+                        {
+                            "Throws a \"Ball of Time\" at Klarg, but Klarg dodges out of the way"
+                        }
+                    }
                 }
             };
 
             return View(model);
-        }
-
-        public List<string> GetTurnOrder()
-        {
-            return new List<string>();
         }
     }
 }
---
+++ The DM's Screen/Models/EncounterFeedModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace The_DM_s_Screen.Models
+{
+    public class EncounterFeedModel
+    {
+        public IEnumerable<Character> TurnOrder;
+        public IEnumerable<EncounterFeedEntry> FeedEntries;
+    }
+
+    public struct EncounterFeedEntry
+    {
+        public Character ActingCharacter;
+        public List<string> Actions;
+    }
+
+    public struct Character
+    {
+        public string Name;
+        public string Portrait;
+    }
+}
